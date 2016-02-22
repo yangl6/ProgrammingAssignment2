f25bffd142d1882ceef43b238cbb3b63ddfb6af4
yangl6 <yliu1382011@gmail.com>
2016-02-22 15:23:33 +1100

    assign2

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,78 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix<-function(m){
+    inverse<-NULL
+    set<-function(y)
+    {
+        m<<-y
+        inverse<<-NULL
+    }
+    
+    get<-function()
+    {
+        m
+    }
+    
+    setInverse<-function(inv)
+    {
+        inverse<<-inv
+    }
+    
+    getInverse<-function()
+    {
+        inverse
+    }
+    
+    list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
 }
 
+cacheSolve<-function(makeFunc)
+{
+    inverse<-makeFunc$getInverse()
+    matrix<-makeFunc$get()
+    
+    if(!is.null(inverse)&&checkIdentity(matrix%*%inverse)==TRUE)
+    {
+        print("Getting the cached Matrix")
+        return(inverse)
+    }
+    
+    result<-solve(matrix)
+    makeFunc$setInverse(result)    
+    result
+}
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##to check if a matrix is identity or not
+checkIdentity<-function(matrix)
+{
+    cols<-ncol(matrix)
+    rows<-nrow(matrix)
+    
+    if(cols!=rows){
+        return(FALSE)
+    }
+    
+    for(i in 1:rows)
+    {
+        
+        for(j in 1:cols)
+        {
+            if(i==j) 
+            {
+                if(round(matrix[i,j],0)!=1)
+                {
+                    return(FALSE);
+                }    
+            }
+            else
+            {
+                if(round(matrix[i,j],0)!=0)
+                {
+                    return(FALSE)
+                }
+            }
+        }
+    }
+    TRUE
 }
