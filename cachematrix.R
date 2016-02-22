0558553306ef968ea8624fde3d88bb0e112d0886
yangl6 <yliu1382011@gmail.com>
2016-02-22 17:23:56 +1100

    Second version of assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -3,11 +3,14 @@
 
 ## Write a short comment describing this function
 
+## This function set and retrieve the original matrix and inverse of it.
+## Arguments: the raw matrix
+## return: a list of internal functions
 makeCacheMatrix<-function(m){
-    inverse<-NULL
+    inverse<-NULL  ## set inversed matrix null initially. 
     set<-function(y)
     {
-        m<<-y
+        m<<-y  ## In different environment, the assignment operator should be <<- instead of <-
         inverse<<-NULL
     }
     
@@ -29,19 +32,25 @@ makeCacheMatrix<-function(m){
     list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
 }
 
+
+## This function computes and returns the inversed matrix, which comes from the original matrix from makeCacheMatrix object.
+## If the inversed matrix has been calculated and the original matrix has not been changed, the cached result should be returned. This is accomplished by checking the inverse matrix is null and the 
+## multiplication of cached inverse and original matrix is identity matrix. Otherwise, the inversed should be computed.
+##Args: makeCacheMatrix object
+##Return: inversed matrix
 cacheSolve<-function(makeFunc)
 {
     inverse<-makeFunc$getInverse()
     matrix<-makeFunc$get()
     
-    if(!is.null(inverse)&&checkIdentity(matrix%*%inverse)==TRUE)
+    if(!is.null(inverse)&&checkIdentity(matrix%*%inverse)==TRUE) ## to check if the inversed matrix is null or not and the matrix has been changed or not.
     {
         print("Getting the cached Matrix")
         return(inverse)
     }
     
-    result<-solve(matrix)
-    makeFunc$setInverse(result)    
+    result<-solve(matrix)  ## to store the inversed matrix to the result variable
+    makeFunc$setInverse(result) ## to update the inversed matrix in the makeCacheMatrix object
     result
 }
 
